553515772bd0bc2c220ea63261edf17ebeaa8870
jadofils <jasezikeye50@gmail.com>
2026-08-18 14:41:10 +0200

    chore(bootstrap): keep HmsApplication in sync with the actuator/metrics dependency changes

--- src/main/java/amalitech/hospital/management/HmsApplication.java
+++ src/main/java/amalitech/hospital/management/HmsApplication.java
@@ -2,12 +2,17 @@ package amalitech.hospital.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class HmsApplication {
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(HmsApplication.class, args);
 
-	public static void main(String[] args) {
-		SpringApplication.run(HmsApplication.class, args);
-	}
-
+        System.out.println("Beans in IoC container:");
+        for (String name : ctx.getBeanDefinitionNames()) {
+            System.out.println(name);
+        }
+    }
 }
+
